1bb9fb90d3f9e78aa8961bcbbf27efc81ac026d4
rootspyro <root.spyro@gmail.com>
2024-07-12 15:50:26 -0400

    Feat: add response for path not found

--- backend/router.ts
+++ backend/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import usersController from "./controllers/users/users.controller";
 import authController from "./controllers/auth/auth.controller";
+import { Request, Response } from "express";
 
 const router: Router = Router()
 
@@ -13,6 +14,14 @@ router.get("/users", authController.ValidAuth, usersController.GetUsers);
 router.get("/users/:userId", authController.ValidAuth, usersController.GetSingleUser);
 router.patch("/users/:userId", authController.ValidAuth, usersController.UpdateUser);
 router.delete("/users/:userId", authController.ValidAuth, usersController.DeleteUser);
+
+// api message 404 NOT FOUND
+router.all("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    error: "path not found"
+  })
+});
 
 
 export default router;
